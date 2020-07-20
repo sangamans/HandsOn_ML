1e721bb0d3f4b9b32a0589284f9ea885437c5e77
Sangaman Senthil <sangaman_senthil@tamu.edu>
2020-07-20 13:54:35 -0500

    loading dataset

--- regression_example/regression_example.py
+++ regression_example/regression_example.py
@@ -1,1 +1,30 @@
+# example from chapter 2 of the book
+# practice with univariate regression
+# dataset based on 1990 California Census - predict a district's median housing price
+import os
+import tarfile
+import urllib
+import pandas as pd
 
+# fetching the data
+DOWNLOAD_ROUTE = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROUTE + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+# loading data to pandas
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+fetch_housing_data()
+housing = load_housing_data()
+print(housing.head())
