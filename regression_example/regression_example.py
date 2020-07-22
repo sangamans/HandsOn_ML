0c81e37b51c46565b99bd2a35206937f14cddcb9
Sangaman Senthil <sangaman_senthil@tamu.edu>
2020-07-22 12:05:57 -0500

    test train split

--- regression_example/regression_example.py
+++ regression_example/regression_example.py
@@ -5,6 +5,10 @@ import os
 import tarfile
 import urllib.request
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 # fetching the data
 DOWNLOAD_ROUTE = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
@@ -31,3 +35,35 @@ def load_housing_data(housing_path=HOUSING_PATH):
 housing = load_housing_data()
 # housing.info() will show description of data and data types
 # houseing.describe() will show summary of statistical attributes
+
+# using matplot lib to study the dataset
+#housing.hist(bins=50, figsize=(20,15))
+#plt.show()
+
+# creating test set - 80% training 20% testing
+
+# how a hard coded function would look like
+# iloc selects rows
+def split_train_test(data, test_ratio):
+    np.random.seed(42)
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+# train_set, test_set = split_train_test(housing, 0.2)
+# use sklearn to efficienlty split training and testing
+# train_set, test_set = train_test_split(housinng, test_size=0.2, random_state=42)
+
+# categorize the incomes
+housing["income_cat"] = pd.cut(housing["median_income"], bins=[0, 1.5, 3.0, 4.5, 6.0, np.inf], labels=[1,2,3,4,5])
+# stratified sampling based on income category
+# why use this instead of normally splitting up the data?
+# when looking at the medium incomes they arent represented equally so the test and training set must be proprtional to that
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+# remove the income_cat attribute so the data is back to original state
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
