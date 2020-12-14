9debaa1caca32e19f648cbaf96443ef2687364ab
Sangaman Senthil <sangaman_senthil@tamu.edu>
2020-12-14 12:23:51 -0600

    new updates

--- classification/classification.py
+++ classification/classification.py
@@ -1,5 +1,5 @@
 # example from chapter 2 of the book
-# practice with classification systems
+# practice with classification systems to see the end to end process of a data science project
 # dataset based MNIST which is a set of 70,000 small digits handwritten by high school students and emplyees of US Census Buraeu
 from sklearn.datasets import fetch_openml
 import numpy as np
